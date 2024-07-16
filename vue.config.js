dc22c3faac3945d4095defb2911d3ccaecddb9df
Manuel Santiago Cabeza <msantiagoc33@gmail.com>
2024-07-16 14:26:30 +0200

    deploy master

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/ifasnet/' : '/',
-  outputDir: 'dist',
-  transpileDependencies: true
+  publicPath: process.env.NODE_ENV === 'production' ? '/ifasnet/' : '/'
 })
